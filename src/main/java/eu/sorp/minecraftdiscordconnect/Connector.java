7d0745adb8291f876735ef9bb2ac1da0048ed789
Ashimara <marvsei@yahoo.de>
2017-06-26 01:16:25 +0200

    removed debug

--- src/main/java/eu/sorp/minecraftdiscordconnect/Connector.java
+++ src/main/java/eu/sorp/minecraftdiscordconnect/Connector.java
@@ -12,7 +12,6 @@ public class Connector {
     
     public static void sendDiscordMessage(String message){
         for(String channelID : MinecraftDiscordConnect.config.sendChannelIDs){
-            System.out.println(Long.parseLong(channelID));
             MinecraftDiscordConnect.client.getChannelByID(Long.parseLong(channelID)).sendMessage(message);
         }
     }
